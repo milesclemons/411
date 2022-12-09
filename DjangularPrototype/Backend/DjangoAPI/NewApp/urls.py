b9d0de18c494ff730637c97caea06b7c0d2e1ac3
molly-yan <68274703+molly-yan@users.noreply.github.com>
2022-12-08 19:28:18 -0500

    Update urls.py
    create paths

--- DjangularPrototype/Backend/DjangoAPI/NewApp/urls.py
+++ DjangularPrototype/Backend/DjangoAPI/NewApp/urls.py
@@ -4,9 +4,10 @@ from django.urls import re_path
 
 
 urlpatterns=[
-
     re_path(r'^employee/$',views.employeeApi),
-    re_path(r'^employee/([0-9]+)$',views.employeeApi)
-    
-
+    re_path(r'^employee/([0-9]+)$',views.employeeApi),
+    re_path(r'^form/$',views.form),
+    re_path(r'^select/$',views.select),
+    re_path(r'^recipe/$', views.recipe),
+    re_path(r'^testing/([0-9]+)$')
 ] 
